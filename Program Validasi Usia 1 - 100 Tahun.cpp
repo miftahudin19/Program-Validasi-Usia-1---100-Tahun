18df7cd8ad18ab405b8b9e7e369313bef4d92df8
miftahudin19 <40747706+miftahudin19@users.noreply.github.com>
2018-12-17 21:55:28 +0700

    Add files via upload

---
+++ Program Validasi Usia 1 - 100 Tahun.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+int umur;
+
+cout<<"PROGRAM MEMVALIDASI UMUR"<<endl;
+
+cout<<"Input tahun kelahiran anda = ";
+cin>>umur;
+umur=2018-umur;
+cout<<"Umur anda adalah      = "<<umur;
+return 0;
+
+
+}
